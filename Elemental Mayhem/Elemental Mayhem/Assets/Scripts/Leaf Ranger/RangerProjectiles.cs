7dda0c85967724ed614a07156aff2b73350df44a
BrendanJamesJackson <brendanjj50@gmail.com>
2026-06-09 17:40:37 +0200

    Finalised Leaf/Tree Ranger

--- Elemental Mayhem/Elemental Mayhem/Assets/Scripts/Leaf Ranger/RangerProjectiles.cs
+++ Elemental Mayhem/Elemental Mayhem/Assets/Scripts/Leaf Ranger/RangerProjectiles.cs
@@ -9,6 +9,7 @@ public class RangerProjectiles : MonoBehaviour
     public GameObject spear;
 
     public Transform shootPoint;
+    public Transform diagonalDirection;
 
     public float shootSpeed;
 
@@ -39,6 +40,61 @@ public class RangerProjectiles : MonoBehaviour
         }
         Destroy(temp, 3f);
 
+    }
+
+    public void FireAirAttack()
+    {
+        GameObject temp = Instantiate(diagonalArrow, shootPoint.position, shootPoint.rotation);
+        Hitbox hb = temp.GetComponent<Hitbox>();
+        if (hb != null)
+        {
+            hb.Initialize(this.gameObject);
+        }
+
+        Rigidbody2D rb = temp.GetComponent<Rigidbody2D>();
+        float direction = this.gameObject.transform.localScale.x;
+        temp.transform.localScale = this.gameObject.transform.localScale;
+        //rb.linearVelocityX = direction * shootSpeed;
+
+        Vector3 tempAngle = diagonalDirection.right;
+        tempAngle.x *= direction;
+        tempAngle.y = -Mathf.Abs(tempAngle.y);
+        //tempAngle.Normalize();
+        Debug.Log(tempAngle);
+        rb.linearVelocity = tempAngle * 45;
+        Destroy(temp, 5f);
+    }
+
+    public void FireElementalAttack2()
+    {
+        GameObject temp = Instantiate(spear, shootPoint.position, shootPoint.rotation);
+        Hitbox hb = temp.GetComponent<Hitbox>();
+        if (hb != null)
+        {
+            hb.Initialize(this.gameObject);
+        }
+
+        Rigidbody2D rb = temp.GetComponent<Rigidbody2D>();
+        float direction = this.gameObject.transform.localScale.x;
+        temp.transform.localScale = this.gameObject.transform.localScale;
+        rb.linearVelocityX = direction * 45;
+        Destroy(temp, 5f);
+    }
+
+    public void FireElementalAttack1()
+    {
+        GameObject temp = Instantiate(thorns, shootPoint.position, shootPoint.rotation);
+        Hitbox hb = temp.GetComponent<Hitbox>();
+        if (hb != null)
+        {
+            hb.Initialize(this.gameObject);
+        }
+
+        Rigidbody2D rb = temp.GetComponent<Rigidbody2D>();
+        float direction = this.gameObject.transform.localScale.x;
+        temp.transform.localScale = this.gameObject.transform.localScale;
+        rb.linearVelocityX = direction * shootSpeed;
+        Destroy(temp, 5f);
     }
 
 
